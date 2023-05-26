b8d548b1d716f08e7c4234b145f7f63bc0a459c4
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-05-26 18:27:01 +0200

    Corrections and exercise completed

--- script.js
+++ script.js
@@ -9,33 +9,35 @@ console.log(target);
 
 // Asking km 
 
-const travelKm = prompt ('Enter how many km to travel', '20');
-console.log(travelKm);
+const travelKm = parseInt(prompt ('Enter how many km to travel', '20'));
+console.log("This are your selected km " + (travelKm));
 
 // Declaration of the cost per km variable
 
 const costPerKm = 0.21;
 const totalKmCost = costPerKm * travelKm;
-console.log(totalKmCost);
+console.log("This is the total km cost " + (totalKmCost));
 
 // Asking user's age
 
-const age = prompt ('Enter your age', '25');
+const age = parseInt(prompt ('Enter your age', '25'));
 console.log(age);
 
 
 // Inserting age variable for discount calculation
 
-let discount = '';
+let discount = 1;
 
 if (age <= 18) {
-    discount = 0.2;
+    discount = 0.8;
 } else if (age >= 65) {
-    discount = 0.4;
+    discount = 0.6;
 }
-console.log("ticket discount: " + (discount * 100) + "%");
+console.log(discount);
 
 
 // calculating ticket price
 
-const ticketPrice = totalKmCost * travelKm;
+const ticketPrice = totalKmCost * discount;
+console.log("ticket price: " + (ticketPrice));
+target.innerText = "Ticket price is: " + (ticketPrice.toFixed(2)) + "€";
